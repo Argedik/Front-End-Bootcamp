03a42d37c63a72490c145c090808d58cf666ac4d
Enes Yasin Gedik <gedikas@hotmail.com>
2022-06-21 14:16:16 +0300

    Update projecteuler_5.js

--- projecteuler_5.js
+++ projecteuler_5.js
@@ -5,16 +5,23 @@
 test(20);
 
 function test(number) {
+  // Arttırılacak sayı,
   increases = 9;
+  // İstenilen sonuç bulunduğunda finalState true olsun ve döngü dursun,
   finalState = false;
+  // Bölünecek sayıyı 1 den başlayıp argüman olarak gelen değere kadar arttır,
   numberState = 1;
   while (finalState === false) {
+    //her dögüye girdiğinde arttırılacak sayıyı 1 arttır
     increases++;
+    //eğer bölümünden kalan 0'sa ve son bölen sayı argüman olarak gelen sayıdan az yada eşitse
     while (numberState <= number && increases % numberState == 0) {
+      //bölen sayı argüman olarak gelen sayıya eşitse döngüyü durdur
       if (numberState == number) {
         finalState = true;
         console.log(increases);
       }
+      //gerisini biliyoruz :)
       numberState++;
     }
     numberState = 1;
